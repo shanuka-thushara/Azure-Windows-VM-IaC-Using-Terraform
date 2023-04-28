42f463aa6a5c101f5d1fa666352a9dbe5306e22d
M ShanukaT <68106560+shanuka-thushara@users.noreply.github.com>
2023-04-28 14:49:20 +0300

    Update Azure-VM-Main.tf
    Add vnet details

--- Azure-VM-Main.tf
+++ Azure-VM-Main.tf
@@ -2,3 +2,11 @@ resource "azurerm_resource_group" "RG" {
   name     = "Test-RG"
   location = "East US"
 }
+
+resource "azurerm_virtual_network" "VNET" {
+  name                = "Test-VNET"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
+}
+
