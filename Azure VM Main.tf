53cb8ef98c459a5bab86c9c0be5f5a33ceb98849
M ShanukaT <68106560+shanuka-thushara@users.noreply.github.com>
2023-04-30 12:30:38 +0300

    Update Azure VM Main.tf
    Azure nic update

--- Azure VM Main.tf
+++ Azure VM Main.tf
@@ -16,3 +16,15 @@ resource "azurerm_subnet" "AzureSubnet" {
   virtual_network_name = azurerm_virtual_network.VNET.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+resource "azurerm_network_interface" "nic" {
+  name                = "Test-nic"
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.AzureSubnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
