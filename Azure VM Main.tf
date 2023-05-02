d0b5db91e8a4a2bd2f8b126b47d9873ca6ce200c
M ShanukaT <68106560+shanuka-thushara@users.noreply.github.com>
2023-05-02 16:31:11 +0300

    Update Azure VM Main.tf
    Create NSG

--- Azure VM Main.tf
+++ Azure VM Main.tf
@@ -36,3 +36,8 @@ resource "azurerm_public_ip" "PIP" {
   allocation_method   = "Static"
   }
 
+resource "azurerm_network_security_group" "NSG" {
+  name                = "Test-NSG"
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
+}
