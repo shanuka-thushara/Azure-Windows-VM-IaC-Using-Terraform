b9864880ac3279f6c64d374447f2ce67dbfe90a1
M ShanukaT <68106560+shanuka-thushara@users.noreply.github.com>
2023-05-03 22:15:18 +0300

    Update Azure VM Main.tf
    Azure vm account and Windows image source

--- Azure VM Main.tf
+++ Azure VM Main.tf
@@ -41,3 +41,27 @@ resource "azurerm_network_security_group" "NSG" {
   location            = azurerm_resource_group.RG.location
   resource_group_name = azurerm_resource_group.RG.name
 }
+
+resource "azurerm_windows_virtual_machine" "VM" {
+  name                = "Test-VM"
+  resource_group_name = azurerm_resource_group.RG.name
+  location            = azurerm_resource_group.RG.location
+  size                = "Standard_B2ms"
+  admin_username      = "azureadmin"
+  admin_password      = "Tf@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-Datacenter"
+    version   = "latest"
+  }
+}
