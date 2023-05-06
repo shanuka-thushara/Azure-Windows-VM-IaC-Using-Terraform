c4f0c869b97cd751d82ba8961ffaafc2a39826ba
M ShanukaT <68106560+shanuka-thushara@users.noreply.github.com>
2023-05-06 18:12:53 +0300

    Update Azure VM Main.tf
    Divided resource blocks

--- Azure VM Main.tf
+++ Azure VM Main.tf
@@ -1,8 +1,10 @@
+# Azure Resource Group
 resource "azurerm_resource_group" "RG" {
   name     = "Test-RG"
   location = "East US"
 }
 
+# Azure Virtual Network
 resource "azurerm_virtual_network" "VNET" {
   name                = "Test-VNET"
   address_space       = ["10.0.0.0/16"]
